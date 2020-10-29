447ff7bcedda168c5212e434f9047d5ca0b0defb
Ben <ben.p.kwok@gmail.com>
2020-10-28 22:53:51 -0700

    Full responsiveness for main page

--- scripts.js
+++ scripts.js
@@ -7,18 +7,9 @@
           x.className = "rightmenu";
         }
       }
-      $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-              $('html,body').animate({
-                  //the 205 is a fixed header offset
-                scrollTop: target.offset().top - 205
-              }, 1000);
-              return false;
-            }
-          }
-        });
-      });
+      function smoothScroll(target, duration) {
+        var target = document.querySelector(target);
+        console.log(target)
+      }
+
+      smoothScroll('#hero', 1000);
